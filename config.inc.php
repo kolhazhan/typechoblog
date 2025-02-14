ae16ea130b79953cb3147f93929886b76e99604e
kolhazhan <124411716+kolhazhan@users.noreply.github.com>
2025-02-14 19:38:48 +0800

    55

---
+++ config.inc.php
@@ -0,0 +1,33 @@
+<?php
+// site root path
+define('__TYPECHO_ROOT_DIR__', dirname(__FILE__));
+
+// plugin directory (relative path)
+define('__TYPECHO_PLUGIN_DIR__', '/usr/plugins');
+
+// theme directory (relative path)
+define('__TYPECHO_THEME_DIR__', '/usr/themes');
+
+// admin directory (relative path)
+define('__TYPECHO_ADMIN_DIR__', '/admin/');
+
+// register autoload
+require_once __TYPECHO_ROOT_DIR__ . '/var/Typecho/Common.php';
+
+// init
+\Typecho\Common::init();
+
+// config db
+$db = new \Typecho\Db('Pdo_Mysql', 'typecho_');
+$db->addServer(array (
+  'host' => 'sql.freedb.tech',
+  'port' => 3306,
+  'user' => 'freedb_kolhazhanchong',
+  'password' => 'YD6Psag*%5ytb3h',
+  'charset' => 'utf8mb4',
+  'database' => 'freedb_kolhazhanchong',
+  'engine' => 'InnoDB',
+  'sslCa' => '',
+  'sslVerify' => true,
+), \Typecho\Db::READ | \Typecho\Db::WRITE);
+\Typecho\Db::set($db);
